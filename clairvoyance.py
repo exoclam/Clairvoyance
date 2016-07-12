eaf1be67c553261069a287f3d58ffeb33f3d7170
Chris Lam <nerevs0805@gmail.com>
2016-07-11 21:42:53 -0400

    clairvoyance

---
+++ clairvoyance.py
@@ -0,0 +1,13 @@
+from hybrid_ann import *
+import sys
+from sys import argv
+
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+script, N_inner, R, P = argv
+N_inner = int(N_inner)
+R = float(R)
+P = float(P)
+P_outer = output(N_inner, R, P)
+print "Clairvoyance predicts a %f percent probability of additional transiting planets with P < 13.7 days." % (P_outer)
